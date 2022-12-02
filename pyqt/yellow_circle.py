1f274c2f957b065817551d89f4d9e65560f0cb50
Кирилл <kirillredko59@gmail.com>
2022-12-02 20:20:23 +0300

    commit

--- pyqt/yellow_circle.py
+++ pyqt/yellow_circle.py
@@ -1,13 +1,34 @@
 import sys
+import random
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QCheckBox, QLineEdit, QPushButton
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtCore import QPoint
 
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('file.ui', self)
+        uic.loadUi('yellow_circle.ui', self)
+        self.button.clicked.connect(self.paint)
+        self.do_paint = False
+
+    def paintEvent(self, event):
+        if self.do_paint is True:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circle(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_circle(self, qp):
+        qp.setBrush(QColor("yellow"))
+        r = random.randint(100, 300)
+        qp.drawEllipse(QPoint(random.randint(200, 600), random.randint(200, 600)), r, r)
 
 
 def except_hook(cls, exception, traceback):
